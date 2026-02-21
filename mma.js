b3c53ac3ea345e9dff2ac2c762991d425b21834a
riteshdeshmukh13 <riteshdeshmukh2513@gmail.com>
2026-02-21 15:17:36 +0530

    alert function added

--- mma.js
+++ mma.js
@@ -9,10 +9,18 @@ const button = document.querySelector("button")
 
 
 button.addEventListener("click", async function(){
+    if (cityElement.value == ""){
+        alert("Please enter the city")
+        return
+    }
     const apiKey ="b3f3ec858bb6c12cc3eb7a872a0ef043";
     const URL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
     const response = await fetch(URL+ cityElement.value + "&appid=" + apiKey )
     var report = await response.json()
+    if (response.status==404){
+        alert("City not found.")
+        return
+    }
     const temp = document.querySelector(".temp")
     temp.innerHTML = report["main"]["temp"]
     const city = document.querySelector(".city")
